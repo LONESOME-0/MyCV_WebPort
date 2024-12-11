2c0113a4d210c96b032d6391545aa1fffdcb4471
LONESOME <pasterbasher@gmail.com>
2024-12-11 22:30:08 +0700

    main

--- src/components/Hero/ImgME.jsx
+++ src/components/Hero/ImgME.jsx
@@ -5,7 +5,7 @@ function ImgME() {
   return (
     <div className='absolute left-[33%] transform -translate-x-1/2  '> 
         <img 
-        src='/public/Img/WerapatImg.jpg' 
+        src='Img/WerapatImg.jpg' 
         alt='ImgWerapat'
         className=' object-cover rounded-full size-[400px]  '
         />
