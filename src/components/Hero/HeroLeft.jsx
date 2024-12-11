e432a7b6757c08130a11dc1e7c388c5d44c85182
LONESOME <pasterbasher@gmail.com>
2024-12-11 22:38:42 +0700

    blank

--- src/components/Hero/HeroLeft.jsx
+++ src/components/Hero/HeroLeft.jsx
@@ -4,14 +4,14 @@ function HeroLeft() {
   return (
     <div className="flex items-end pl-16 pb-16 w-1/3 bg-amber-700 h-screen">
       <div className='text-3xl uppercase' >Follow Me :</div>
-        <a href='https://www.linkedin.com/in/werapat-rommalee-64877a338/'>
+        <a href='https://www.linkedin.com/in/werapat-rommalee-64877a338/' target="_blank" >
         <img 
         src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-plain.svg"
         alt='iconlinkin'
         className='w-[67px] h-[39px]'
         />  
         </a>
-        <a href='https://github.com/LONESOME-0?tab=repositories'>
+        <a href='https://github.com/LONESOME-0?tab=repositories' target="_blank">
         <img 
         src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg"
         alt='icongithub'
